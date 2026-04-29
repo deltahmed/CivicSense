2611ac1670cc825a5ea16af68a82b77f4b353d89
deltahmed <fantasi.jump@gmail.com>
2026-04-29 19:55:02 +0200

    Ajout routes profil utilisateur

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from .views import (
+    GetPublicUserView,
+)
 
-urlpatterns = []
+urlpatterns = [
+    path('<int:pk>/', GetPublicUserView.as_view(), name='public-user-detail'),
+]
 
 
